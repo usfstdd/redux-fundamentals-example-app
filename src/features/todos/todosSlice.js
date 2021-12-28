b5a1a7568546e0eeb3282df3071b5ba9f7c96053
yousef stdd <usf.stdd@gmail.com>
2021-12-28 16:01:44 +0330

    redux apps have a root reducer function, that takes previous state, and action and returns new state.alse we will pass it to a function, and store object will be created. to "reducer" function be readable and maintaiable we must split "root reducer" function into file and folders based on "features" as redux documentation suggests.
    to code be easily predictable and so we can use predictablity benefits (like: easier debbuging, use DevTools,... ) reducer function must be
    "pure" function.
    
    "pure" functions must follow these ruls:
    1. function's outout must only calculated based on function's argument
    2. updates must be immutable (in the case of JS this can be handled by spread operator)
    3. no async logic or other side effects

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,55 @@
+const initiaState = [{
+    id: 0,
+    text: 'whatch colte steel web programming course and get insights..',
+    completed: true,
+    color: 'red'
+}, {
+    id: 1,
+    text: 'read MDN documentations of js to fully understand the concepts of JS and see how this language evolved and why?',
+    completed: false,
+    color: 'purple'
+}, {
+    id: 2,
+    text: 'learn a framework like react',
+    completed: false,
+    color: 'blue'
+}, ]
+
+
+function nextTodoID(todos) {
+    todos.reduce((newItemID, item) => Math.max(newItemID, item.id), 0)
+}
+
+export default function todosReducer(state = initiaState, action) {
+    switch (action.type) {
+        case 'todos/todoAdded': {
+            return [
+                ...state,
+                {
+                    id: nextTodoID(state.todos),
+                    text: action.payload,
+                    completed: false,
+
+                }
+
+            ]
+
+        }
+        case 'todos/todoToggled': {
+            return state.map(todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    }
+                }
+                return todo;
+
+            })
+
+        }
+
+        default:
+            return state;
+    }
+}
