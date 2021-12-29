5fb1bf1f693d97b45734800b64e132b270088499
yousef stdd <usf.stdd@gmail.com>
2021-12-29 19:11:34 +0330

    work with store API. that's is and object that contains thee function dispatch, getState, subscribe. the inner functionality of this API is simple. these three functons have to two variable named state and listeners.
    subscribe added new listener callbacks to listeners array.
    getState returns the state.
    dispatch, calles the reudcer function, reassigns new vlaue for state and
    instead a loop calls all listener callbacks.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import store from "./store";
+console.log("initial State is:", store.getState())
 
-import './api/server'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const unSubscribe = store.subscribe(() =>
+  console.log('store after dispatch', store.getState())
 )
+
+store.dispatch({
+  type: "todos/todoAdded",
+  payload: 'read these book that jadi suggested'
+})
+
+store.dispatch({
+  type: "todos/todoAdded",
+  payload: 'see the jadi\'s network course '
+})
+
+store.dispatch({
+  type: "todos/todoAdded",
+  payload: 'watch the don\'t loo up movie'
+})
+
+store.dispatch({
+  type: 'todos/todoToggled',
+  payload: 3
+})
+
+store.dispatch({
+  type: 'todos/todoToggled',
+  payload: 1
+})
+
+store.dispatch({
+  type: 'filter/statusFilterChanged',
+  payload: {
+    color: 'red',
+    changeType: 'added'
+  }
+})
+
+store.dispatch({
+  type: 'filter/statusFilterChanged',
+  payload: 'Active'
+})
+
+unSubscribe();
+store.dispatch({
+  type: 'todos/todoAdded',
+  payload: 'Try creating a store'
+})
