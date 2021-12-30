fbc1636209432a8826fc482886c6eb62602e3854
yousef stdd <usf.stdd@gmail.com>
2021-12-30 13:26:45 +0330

    with use of store enhancers we can change how the store API behaves by wrapping around the original Redux stor.
    redux enhancer is a funtion that takes createStore as argument and
    returns a function with
    1. root reudcer
    2. preloadedState
    3. enhancer
    as argument. and creates the store and returns {...store, other properties}
    the other properties can be dispach, getState, subscribe (the original
    properties of store object's API); in other words we are chnaging the
    behaviour of original store.

--- src/store.js
+++ src/store.js
@@ -2,6 +2,10 @@ import {
     createStore
 } from "redux";
 import rootReducer from "./reducer";
+import {
+    sayHiOnDispatch
+} from "./exampleAddons/enhancers";
 
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, undefined, sayHiOnDispatch);
 export default store;
