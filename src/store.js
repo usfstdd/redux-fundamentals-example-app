f6dcb384ab3e4712100cbc51ece29789107bf5ef
yousef stdd <usf.stdd@gmail.com>
2021-12-30 18:04:49 +0330

    use of composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -4,10 +4,18 @@ import {
 } from "redux";
 import rootReducer from "./reducer";
 import {
+    composeWithDevTools
+} from "redux-devtools-extension";
+
+
+import {
     print1,
     print2,
     print3
 } from "./exampleAddons/middleware";
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
-const store = createStore(rootReducer, undefined, middlewareEnhancer);
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(print1, print2, print3)
+)
+
+const store = createStore(rootReducer, undefined, composedEnhancer);
 export default store;
